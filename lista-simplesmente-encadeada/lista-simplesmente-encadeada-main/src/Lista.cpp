3a350b42615749938afdf87ecdcc794c4adb4410
MrHuguitos <155857650+MrHuguitos@users.noreply.github.com>
2026-05-16 20:09:40 +0000

    Resolução dos exercícios

--- lista-simplesmente-encadeada/lista-simplesmente-encadeada-main/src/Lista.cpp
+++ lista-simplesmente-encadeada/lista-simplesmente-encadeada-main/src/Lista.cpp
@@ -12,13 +12,12 @@ Lista::Lista()
 
 /**
  * @brief Destrutor da lista. Remove todos os nós alocados dinamicamente.
- */
-Lista::~Lista()
-{
+*/
+
+Lista::~Lista() {
     auto aux = this->primeiro;
 
-    while(aux != nullptr)
-    {
+    while(aux != nullptr) {
         auto tmp = aux;
         aux = aux->proximo;
         delete tmp;
@@ -34,10 +33,19 @@ Lista::~Lista()
  * 
  * @param elemento Elemento a ser inserido.
  * @return true se inserido com sucesso.
- */
-bool Lista::inserirInicio(const std::string& elemento)
-{
-    throw std::runtime_error("Ainda não foi implementado.");
+*/
+
+bool Lista::inserirInicio(const std::string& elemento) {
+    No* novoElemento = new No(elemento);
+
+    if (this->quantidade == 0) this->ultimo = novoElemento;
+    
+    novoElemento->proximo = this->primeiro;
+    this->primeiro = novoElemento;
+
+    this->quantidade++;
+
+    return true;
 }
 
 /**
@@ -45,10 +53,20 @@ bool Lista::inserirInicio(const std::string& elemento)
  * 
  * @param elemento Elemento a ser inserido.
  * @return true se inserido com sucesso.
- */
-bool Lista::inserirFim(const std::string& elemento)
-{
-    throw std::runtime_error("Ainda não foi implementado.");
+*/
+
+bool Lista::inserirFim(const std::string& elemento) {
+    No* novoElemento = new No(elemento);
+
+    if (this->quantidade == 0) this->primeiro = novoElemento;
+    else this->ultimo->proximo = novoElemento;
+
+    novoElemento->proximo = nullptr;
+    this->ultimo = novoElemento;
+
+    this->quantidade++;
+    
+    return true;
 }
 
 /**
@@ -58,10 +76,26 @@ bool Lista::inserirFim(const std::string& elemento)
  * @param elemento Elemento a ser inserido.
  * @return true se a inserção for realizada com sucesso.
  * @throws std::out_of_range se o índice passado como parâmetro for menor do que 1 ou maior do que o tamanho+1.
- */
-bool Lista::inserirNaPosicao(int i, const std::string& elemento) 
-{
-    throw std::runtime_error("Ainda não foi implementado.");
+*/
+
+bool Lista::inserirNaPosicao(int i, const std::string& elemento) {
+    if (i < 1 || i > this->quantidade + 1) throw std::out_of_range("Índice Inválido");
+    if (i == 1) return Lista::inserirInicio(elemento);
+    if (i == this->quantidade + 1) return Lista::inserirFim(elemento);
+    
+    No* novoElemento = new No(elemento);
+    auto anterior = this->primeiro;
+
+    for (int j = 1; j < i - 1; j++) {
+        anterior = anterior->proximo;
+    }
+
+    novoElemento->proximo = anterior->proximo;
+    anterior->proximo = novoElemento;
+
+    this->quantidade++;
+
+    return true;
 }
 
 /**
@@ -69,10 +103,24 @@ bool Lista::inserirNaPosicao(int i, const std::string& elemento)
  * 
  * @return true se o elemento for removido com sucesso.
  * @throws std::out_of_range se a lista estiver vazia.
- */
-bool Lista::removerInicio()
-{
-    throw std::runtime_error("Ainda não foi implementado.");
+*/
+
+bool Lista::removerInicio() {
+    if (this->quantidade == 0) throw std::underflow_error("Lista vazia");
+
+    auto elemento = this->primeiro;
+
+    if (this->quantidade == 1) {
+        this->primeiro = nullptr;
+        this->ultimo = nullptr;
+    } 
+    else this->primeiro = elemento->proximo;
+
+    delete elemento;
+
+    this->quantidade--;
+
+    return true;
 }
 
 /**
@@ -80,10 +128,33 @@ bool Lista::removerInicio()
  * 
  * @return true se o elemento for removido com sucesso.
  * @throws std::out_of_range se a lista estiver vazia.
- */
-bool Lista::removerFim()
-{
-    throw std::runtime_error("Ainda não foi implementado.");
+*/
+
+bool Lista::removerFim() {
+    if (this->quantidade == 0) throw std::underflow_error("Lista vazia");
+
+    auto elemento = this->ultimo;
+
+    if (this->quantidade == 1) {
+        this->ultimo = nullptr;
+        this->primeiro = nullptr;
+    }
+    else {
+        auto anterior = this->primeiro;
+
+        for (int i = 1; i < this->quantidade - 1; i++) {
+            anterior = anterior->proximo;
+        }
+
+        anterior->proximo = nullptr;
+        this->ultimo = anterior;
+    }
+
+    delete elemento;
+
+    this->quantidade--;
+
+    return true;
 }
 
 /**
@@ -92,10 +163,28 @@ bool Lista::removerFim()
  * @param i Posição do elemento a ser removido.
  * @return true se o elemento for removido com sucesso.
  * * @throws std::out_of_range se o índice passado como parâmetro for menor do que 1 ou maior do que o tamanho.
- */
-bool Lista::removerNaPosicao(int i) 
-{
-    throw std::runtime_error("Ainda não foi implementado.");
+*/
+
+bool Lista::removerNaPosicao(int i) {
+    if (this->quantidade == 0) throw std::underflow_error("Lista vazia");
+    if (i < 1 || i > this->quantidade) throw std::out_of_range("Índice Inválido");
+    if (i == 1) return Lista::removerInicio();
+    if (i == this->quantidade) return Lista::removerFim();
+    
+    auto anterior = this->primeiro;
+
+    for (int j = 1; j < i - 1; j++) {
+        anterior = anterior->proximo;
+    }
+
+    auto elemento = anterior->proximo;
+    anterior->proximo = elemento->proximo;
+
+    delete elemento;
+
+    this->quantidade--;
+
+    return true;
 }
 
 /**
@@ -103,15 +192,13 @@ bool Lista::removerNaPosicao(int i)
  * 
  * @return Referência constante para a string armazenada no primeiro nó.
  * @throws std::out_of_range se a lista estiver vazia.
- */
-const std::string& Lista::primeiroElemento() const
-{
-    if (quantidade > 0)
-    {
+*/
+
+const std::string& Lista::primeiroElemento() const {
+    if (quantidade > 0) {
         return primeiro->valor;
     }
-    else
-    {
+    else {
         throw std::out_of_range("A lista está vazia. Não é possível acessar o primeiro elemento.");
     }
 }
@@ -121,15 +208,13 @@ const std::string& Lista::primeiroElemento() const
  * 
  * @return Referência constante para a string armazenada no último nó.
  * @throws std::out_of_range se a lista estiver vazia.
- */
-const std::string& Lista::ultimoElemento() const
-{
-    if (quantidade > 0)
-    {
+*/
+
+const std::string& Lista::ultimoElemento() const {
+    if (quantidade > 0) {
         return ultimo->valor;
     }
-    else
-    {
+    else {
         throw std::out_of_range("A lista está vazia. Não é possível acessar o último elemento.");
     }
 }
@@ -140,32 +225,42 @@ const std::string& Lista::ultimoElemento() const
  * @param i Índice do elemento a ser acessado.
  * @return Referência constante para a string armazenada na posição i.
  * @throws std::out_of_range se o índice passado como parâmetro for menor do que 1 ou maior do que o tamanho.
- */
-const std::string& Lista::elementoNaPosicao(int i) const 
-{
-    throw std::runtime_error("Ainda não foi implementado.");
+*/
+
+const std::string& Lista::elementoNaPosicao(int i) const {
+    if (this->quantidade == 0) throw std::underflow_error("Lista vazia");
+    if (i < 1 || i > this->quantidade) throw std::out_of_range("Índice Inválido");
+    if (i == 1) return Lista::primeiroElemento();
+    if (i == this->quantidade) return Lista::ultimoElemento();
+
+    auto elemento = this->primeiro;
+
+    for (int j = 1; j < i; j++) {
+        elemento = elemento->proximo;
+    }
+    
+    return elemento->valor;
 }
 
 /**
  * @brief Retorna o número de elementos armazenados na lista.
  * 
  * @return Quantidade de elementos.
- */
-unsigned long Lista::tamanho() const
-{
+*/
+
+unsigned long Lista::tamanho() const {
     return quantidade;
 }
 
 /**
  * @brief Imprime os elementos da lista no formato: { elem1, elem2, ... }
- */
-std::string Lista::imprimir() const
-{
+*/
+
+std::string Lista::imprimir() const {
     std::ostringstream oss;
     oss << "{";
     No* atual = primeiro;
-    while (atual != nullptr)
-    {
+    while (atual != nullptr) {
         oss << atual->valor;
         if (atual->proximo != nullptr)
             oss << ", ";
@@ -180,18 +275,55 @@ std::string Lista::imprimir() const
  * 
  * @param valor Valor a ser removido da lista.
  * @return Quantidade de nós removidos.
- */
-int Lista::removerTodos(const std::string& valor)
-{
-    throw std::runtime_error("Ainda não foi implementado.");
+*/
+
+int Lista::removerTodos(const std::string& valor) {
+    if (this->quantidade == 0) throw std::out_of_range("Lista vazia");
+    if (this->quantidade == 1) {
+        if (this->primeiro->valor == valor) {
+            delete this->primeiro;
+
+            this->primeiro = nullptr;
+            this->ultimo = nullptr;
+            this->quantidade--;
+
+            return 1;
+        }
+        else return 0;
+    }
+
+    int nos_removidos = 0;
+    auto atual = this->primeiro; // Nó no início da fila
+    No* anterior = nullptr; // Nó antes do início, no vazio
+
+    while (atual != nullptr) {
+        if (atual->valor == valor) { // Se o valor do nó atual for igual ao valor que queremos resolver
+            auto proximo = atual->proximo;
+
+            if (anterior == nullptr) this->primeiro = proximo; // Caso o valor anterior seja nulo, o próximo elemento se torna o primeiro
+            else anterior->proximo = proximo; 
+
+            if (atual == this->ultimo) this->ultimo = anterior; // Caso o valor atual seja o último, seu anterior se torna o último
+
+            delete atual;
+            this->quantidade--;
+            nos_removidos++;
+            
+            atual = proximo;
+        }
+        else {
+            anterior = atual;
+            atual = atual->proximo;
+        } 
+    }
+
+    return nos_removidos;
 }
 
-Lista::No::No(std::string valor)
-{
+Lista::No::No(std::string valor) {
     this->valor = valor;
     this->proximo = nullptr;
 }
 
-Lista::No::~No()
-{
+Lista::No::~No() {
 }
